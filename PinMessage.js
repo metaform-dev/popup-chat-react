35e2ba72437af20311aca4e6c467f98e752d3294
metaform-dev <116716613+metaform-dev@users.noreply.github.com>
2022-10-27 18:17:48 +0800

    update pin message

---
+++ PinMessage.js
@@ -0,0 +1,57 @@
+import { pipe, prop, propOr, length, ifElse } from 'ramda';
+import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../assets/image.png';
+
+function PinMessage({ pinMessage, onPinMessage }) {
+
+  const ticketStatus = pinMessage.status;
+  const ticketId = pinMessage._id;
+
+  const renderTicket = () => {
+    if (ticketStatus == "New" || ticketStatus == "Ongoing") {
+      // show ticket
+      return (
+        <>
+          <div className='sc-pin--message--desc'>
+            <div className='sc-pin--message--title'>{`Ticket - ${ticketId}`}</div>
+          </div>
+          <button style={{ float: "right", height: "40px", width: "120px", color: "white", borderRadius: "10px", fontSize: "14px", fontWeight: 700, fontFamily: 'Ubuntu', backgroundColor:ticketStatus == "New"?"#096191":"#c46fb2", border: "solid 0px" }}>{ticketStatus}</button>
+        </>
+      )
+
+    } else {
+      // create
+      return (
+        <>
+          <div className='sc-pin--message--desc'>
+            <div className='sc-pin--message--title'>If you have any question, please create a new ticket.</div>
+          </div>
+          <button style={{ float: "right", height: "40px", width: "200px", cursor: "pointer", color: "white", borderRadius: "10px", fontSize: "14px", fontWeight: 700, fontFamily: 'Ubuntu', backgroundColor: "#459BFF", border: "solid 0px" }} onClick={() => onPinMessage(pinMessage)}>+ Create Ticket</button>
+        </>
+      )
+    }
+  }
+
+  const title = pipe(
+    prop('title'),
+    ifElse(
+      item => length(item) > 34,
+      item => `${item.slice(0, 34)}...`,
+      item => item,
+    )
+  )(pinMessage);
+
+  return (
+    <div className='sc-pin--message' >
+      {renderTicket()}
+    </div>
+  );
+}
+
+PinMessage.propTypes = {
+  pinMessage: PropTypes.object,
+  onPinMessage: PropTypes.func,
+};
+
+export default PinMessage;
