f95a10a2d4503f6d324fa8b14380ec9b4ea2dd1d
metaform-dev <116716613+metaform-dev@users.noreply.github.com>
2022-11-01 18:56:07 +0800

    Update MessageList.js
    add preset note

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -2,6 +2,12 @@ import { prop } from 'ramda';
 import React, { useRef, useEffect } from 'react';
 import Message from './Messages';
 
+const renderreplyNote = (message) => {
+  if (message.author == "me") {
+    return <div style ={{color:"darkgray", fontSize:12, textAlign:"center", marginTop:20}}>--- We will reply to you as soon as possible ---</div>
+  }
++}
+
 function MessageList(props) {
   const element = useRef(null);
   const elementCurrent = prop('current', element);
@@ -15,6 +21,7 @@ function MessageList(props) {
   return (
     <div className="sc-message-list" ref={element}>
       {props.messages.map((message, i) => <Message message={message} key={i} />)}
+      {renderreplyNote(props.messages.at(-1))}
     </div>
   );
 }
