eb1dd3a1c12368548907588101913164dc0a12d3
metaform-dev <dev@metaform.xyz>
2022-11-24 16:46:15 +0800

    Fix launcher icon url

--- src/components/LauncherNew.js
+++ src/components/LauncherNew.js
@@ -95,8 +95,8 @@ function LauncherNew(props) {
     <div id="sc-launcher">
 	    <div className={classNames('sc-launcher', { 'opened': state.isOpen })} onClick={handleClick}>
 		    <MessageCount count={newMessagesCount} isOpen={state.isOpen} />
-		    <img className={'sc-open-icon'} src={openIconUrl} />
-		    <img className={'sc-closed-icon'} src={closeIconUrl} />
+		    <img className={'sc-open-icon'} src={closeIconUrl} />
+		    <img className={'sc-closed-icon'} src={openIconUrl} />
 	    </div>
 
 	    <ChatWindow
