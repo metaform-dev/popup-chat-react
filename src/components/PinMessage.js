f9de07f1b5ad5c779d9ae44d9c22c90b106b372c
metaform-dev <dev@metaform.xyz>
2022-12-21 13:44:59 +0800

    Fix ticket status

--- src/components/PinMessage.js
+++ src/components/PinMessage.js
@@ -18,20 +18,19 @@ function PinMessage(props) {
     if (ticketStatus == "New" || ticketStatus == "Ongoing") {
       // show ticket
       return (
-        <div style={{display:"flex", width:"100%", justifyContent:"space-between"}}>
-          {pinLeftButton? pinLeftButton:""}
+        <div style={{ display: "flex", width: "100%", justifyContent: "space-between" }}>
+          {pinLeftButton ? pinLeftButton : ""}
           <div className='sc-pin--message--desc'>
             <div className='sc-pin--message--title'>{`Ticket - ${ticketId}`}</div>
           </div>
-          <button style={{ float: "right", height: "40px", maxWidth: "20%", cursor: "pointer", color: "white", borderRadius: "10px", fontSize: "14px", fontWeight: 700, fontFamily: 'Ubuntu', backgroundColor: "#459BFF", border: "solid 0px" }} onClick={() => onPinMessage(pinMessage)}>{pinMessageButtonText}</button>
-        </div>
+          <button style={{ float: "right", height: "40px", width: "120px", color: "white", borderRadius: "10px", fontSize: "14px", fontWeight: 700, fontFamily: 'Ubuntu', backgroundColor: ticketStatus == "New" ? "#096191" : "#c46fb2", border: "solid 0px" }}>{ticketStatus}</button>        </div>
       )
 
     } else {
       // create
       return (
-        <div style={{display:"flex", width:"100%", justifyContent:"space-between"}}>
-          {pinLeftButton? pinLeftButton:""}
+        <div style={{ display: "flex", width: "100%", justifyContent: "space-between" }}>
+          {pinLeftButton ? pinLeftButton : ""}
           <div className='sc-pin--message--desc'>
             <div className='sc-pin--message--title'>{pinMessagePlaceholder}</div>
           </div>
